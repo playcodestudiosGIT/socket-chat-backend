cf1410427822eacfa5ea0c78e489ec7df27d7d8d
Playcode <playcodestudios@outlook.com>
2022-05-08 13:42:00 -0500

    fin seccion 8

---
+++ database/config.js
@@ -0,0 +1,25 @@
+const { process_params } = require('express/lib/router');
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+
+    try {
+
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('DB online');
+
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error en la base de datos - Hable con el admin');
+    }
+
+}
+
+module.exports = {
+    dbConnection
+}
